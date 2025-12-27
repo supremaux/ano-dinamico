5cfcb90b3257723a7a10fe6024c5b76e36cec082
Suprema UX <70323230+supremaux@users.noreply.github.com>
2025-12-27 08:03:30 -0400

    Adição do PHP

---
+++ ano-wordpress.php
@@ -0,0 +1,6 @@
+<?php
+    $ano = date('Y');
+    echo "&copy; $ano Ano dinâmico - Todos os direitos reservados | Desenvolvido por Suprema";
+?>
+
+<p>&copy; <?php echo date('Y')?> Ano dinâmico - Todos os direitos reservados | Desenvolvido por Suprema</p>
